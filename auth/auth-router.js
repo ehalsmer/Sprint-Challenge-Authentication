1ff1a7dabd08ceab4282558ea83a1632cd7207fc
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-20 09:40:42 -0600

    Login functionality complete on /api/auth/login

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -24,8 +24,19 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  // implement login
-});
+  let { username, password} = req.body;
+  Users.findBy({ username }).first()
+  .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)){
+          const token = generateToken(user);
+          res.status(200).json({message: `Welcome, ${user.username}`, token});
+      } else {
+          res.status(401).json({message: 'Invalid username and/or password'})
+      }
+  })
+  .catch(err => {
+      res.status(500).json({message: 'Error finding user'})
+  })});
 
 function generateToken(user){
   const payload = {
